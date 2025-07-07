a861b55626813cdecffcbf5485bd7803ee462243
Matheus <mathrocha335@gmail.com>
2025-07-07 11:26:08 -0300

    base page layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
